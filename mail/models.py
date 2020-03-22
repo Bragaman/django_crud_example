26f483d0609efa4f428ff6fe96aadab00f91a99d
Bragaman <cwbraga70@live.com>
2020-03-22 16:42:06 +0300

    Add mail model and app

---
+++ mail/models.py
@@ -0,0 +1,42 @@
+from django.contrib.postgres.indexes import GinIndex
+from django.db import models
+
+
+class Mail(models.Model):
+    user_from = models.ForeignKey(
+        "auth.User",
+        null=False,
+        related_name="sent_mail",
+        verbose_name="From",
+        on_delete=models.CASCADE,
+    )
+    user_to = models.ForeignKey(
+        "auth.User",
+        null=False,
+        related_name="received",
+        verbose_name="To",
+        on_delete=models.CASCADE,
+    )
+    subject = models.TextField(
+        blank=False,
+        null=False,
+        verbose_name="Subject",
+    )
+    text = models.TextField(
+        verbose_name="Text"
+    )
+    created_at = models.DateTimeField(
+        auto_created=True
+    )
+
+    class Meta:
+        ordering = ["-pk"]
+        indexes = [
+            models.Index(fields=['user_from']),
+            models.Index(fields=['user_to']),
+            models.Index(fields=['created_at']),
+            GinIndex(fields=['subject'])
+        ]
+
+        verbose_name = "Mail"
+        verbose_name_plural = "Mails"
